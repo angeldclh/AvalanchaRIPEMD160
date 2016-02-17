d5565adf161d2a0d5d7c0e19830aaa9f2ff0aab0
Ángel del Castillo Las Heras <angel7delcastillo@gmail.com>
2016-02-17 10:49:06 +0100

    Modificaciones GOST -> RIPEMD-160
    Cambiadas variables, número componentes array dist. Hamming, etc.

--- avalancha.c
+++ avalancha.c
@@ -5,82 +5,67 @@
 
 
 int hammingDist(unsigned char *a, unsigned char *b){
-	return 3;
+  return 3;
 }
 
 int main(int argc, char **argv){
 
-	// Comprobar número argumentos
-	if(argc != 1 && argc != 2){
-		fprintf(stderr, "Número incorrecto de argumentos.\n./avalancha\n./avalancha numIteraciones\n");
-		exit(1);
-	}
+  // Comprobar número argumentos
+  if(argc != 1 && argc != 2){
+    fprintf(stderr, "Número incorrecto de argumentos.\n./avalancha\n./avalancha numIteraciones\n");
+    exit(1);
+  }
 
-	// Hayar número iteraciones y bits del texto y de la clave a cambiar en cada iteración
-	int numIteraciones;
-	if(argc == 1){
-		numIteraciones = 50; //PONER ESTE VALOR BIEN, ESTÁ INVENTADO. TAMBIÉN EN EL PRINTF
-		printf("No se especifican argumentos, se asumen 50 iteraciones.\n");
-	}
-	else{
-		numIteraciones = atoi(argv[1]);
-	}
+  // Hayar número iteraciones y bits del texto y de la clave a cambiar en cada iteración
+  int numIteraciones;
+  if(argc == 1){
+    numIteraciones = 50; //PONER ESTE VALOR BIEN, ESTÁ INVENTADO. TAMBIÉN EN EL PRINTF
+    printf("No se especifican argumentos, se asumen 50 iteraciones.\n");
+  }
+  else{
+    numIteraciones = atoi(argv[1]);
+  }
 
 
-	// Buffers donde guardar los bits aleatorios generados (64 bits = 8 bytes)
-	unsigned char *x = (unsigned char*)malloc(8);
-	unsigned char *x2 = (unsigned char*)malloc(8);
-	unsigned char *y = (unsigned char*)malloc(8);
-	unsigned char *y2 = (unsigned char*)malloc(8);
-	unsigned char *y3 = (unsigned char*)malloc(8);
+  // Buffers donde guardar los bits aleatorios generados (VER LONGITUD) y sus hashes
+  unsigned char *x = (unsigned char*)malloc(8);
+  unsigned char *h = (unsigned char*)malloc(20); //hash: 160b=20B. Ver si es unsigned char o qué
+  unsigned char *x2 = (unsigned char*)malloc(8);
+  unsigned char *h2 = (unsigned char*)malloc(20); //hash: 160b=20B. Ver si es unsigned char o qué
 
-	// Buffers donde guardar las claves (256 bits = 32 bytes)
-	unsigned char *k = (unsigned char*)malloc(32);
-	unsigned char *k2 = (unsigned char*)malloc(32);
 
-	// Arrays con contadores para las distancias de Hamming
-	int contDHC[65], contDHK[257];
+  // Arrays con contadores para las distancias de Hamming
+  int contDH[161];
 
-	int i, j, dhc, dhk;
-	for(i=0;i<numIteraciones;i++){
-		// Calcular aleatoriamente la clave y el primer número
-		RAND_bytes(k, 32);
-		RAND_bytes(x, 8);
-		// Alterar 1 bit de k y x para obtener k2 y x2
-		*k2 = *k ^ 1 << (i%256);
-		*x2 = *x ^ 1 << (i%64);
-		// Cifrar x y x2 con la clave k y x con la clave k2
-		/*y = gost(x, k);
-		y2 = gost(x2, k);
-		y3 = gost(x, k2);*/
-		// Calcular distancias de Hamming
-		dhc = hammingDist(y, y2);
-		dhk = hammingDist(y, y3);
-		// Actualizar contadores
-		for(j=0;j<65;j++){
-			if (dhc==j) contDHC[j]++;
-		}
-		for(j=0;j<257;j++){
-			if (dhk==j) contDHK[j]++;
-		}
-	}
+  int i, j, dh;
+  for(i=0;i<numIteraciones;i++){
+    // Calcular aleatoriamente el primer número
+    RAND_bytes(x, 8);
+    // Alterar 1 bit de x para obtener x2
+    *x2 = *x ^ 1 << (i%160);
+    // Hash de x y de x2
+    /*h = hash(x);
+      h2 = hash(x2);
+    */
+    // Calcular distancias de Hamming
+    dhc = hammingDist(h, h2);
+    // Actualizar contadores
+    for(j=0;j<161;j++){
+      if (dh==j) contDH[j]++;
+    }
+    
+  }
 
-	// Imprimir contadores
-	for(j=0;j<65;j++){
-		printf("Distancia Hamming entre criptogramas alterando 1 bit del texto = %d: %d veces.\n", j, contDHC[j]);
-	}
-	for(j=0;j<257;j++){
-		printf("Distancia Hamming entre criptogramas alterando 1 bit de la clave = %d: %d veces.\n", j, contDHK[j]);
-	}
+  // Imprimir contadores
+  for(j=0;j<161;j++){
+    printf("Distancia Hamming entre hashes alterando 1 bit del texto = %d: %d veces.\n", j, contDH[j]);
+  }
 
-	// Liberar memoria asignada
-	free(x);
-	free(x2);
-	free(y);
-	free(y2);
-	free(y3);
-	free(k);
-	free(k2);
+  // Liberar memoria asignada
+  free(x);
+  free(x2);
+  free(h);
+  free(h2);
 
-	return 0;
+  return 0;
 }
