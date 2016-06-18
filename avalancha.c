41a0dbc97b5ba73a92ac46d5d856aed8d8b17449
Ángel <angel7delcastillo@gmail.com>
2016-06-18 14:52:51 +0200

    mejora final bucle
    Número bits del teto * numIteraciones iteraciones en total. Mejora contadores dist Hamming

--- avalancha.c
+++ avalancha.c
@@ -5,6 +5,8 @@
 #include <openssl/evp.h>
 #include <string.h>
 
+#define NUMBITS 160   //Número de bits del texto en claro
+
 // Calcular distancia de Hamming de dos strings de igual tamaño: número de 1s de su XOR a nivel de bit
 int hammingDist(unsigned char *a, unsigned char *b){ //REVISAR
 
@@ -38,39 +40,45 @@ int main(int argc, char **argv){
 
 
   // Buffers donde guardar los bits aleatorios generados (VER LONGITUD) y sus hashes
-  unsigned char *x = (unsigned char*)malloc(8);
+  unsigned char *x = (unsigned char*)malloc(NUMBITS/8);
   unsigned char *h = (unsigned char*)malloc(20); //hash: 160b=20B. Ver si es unsigned char o qué
-  unsigned char *x2 = (unsigned char*)malloc(8);
+  unsigned char *x2 = (unsigned char*)malloc(NUMBITS/8);
   unsigned char *h2 = (unsigned char*)malloc(20); //hash: 160b=20B. Ver si es unsigned char o qué
 
 
-  // Arrays con contadores para las distancias de Hamming
-  int *distancias = (int*) malloc(numIteraciones*sizeof(int));
+  /*distancias[z] contiene las veces que la distancia de Hamming entre hashes ha sido igual a z. Al tener el hash 160 bits,
+    la distancia estará siempre en el intervalo [0,160]*/
+  int *distancias = (int*) malloc(161*sizeof(int));
 
+  int itsTotales = numIteraciones*NUMBITS;
   int i, totalDH;
-  for(i=0;i<numIteraciones;i++){
+  /* Cada iteración altera un bit distinto del texto en claro. Tantas como indique el parámetro * nº bits de texto
+     en claro (NUMBITS)*/
+  for(i=0;i<itsTotales;i++){
     // Calcular aleatoriamente el número
-    RAND_bytes(x, 8);
-    // Alterar 1 bit de x para obtener x2. En cada iteración uno distinto: numIteraciones mejor que sea múltiplo de 160
-    *x2 = *x ^ 1 << (i%160);
+    RAND_bytes(x, NUMBITS/8);
+    // Alterar 1 bit de x para obtener x2. 
+    *x2 = *x ^ 1 << (i%NUMBITS);
     // Hash de x y de x2
     /*h = hash(x);
       h2 = hash(x2);
     */
     // Calcular distancia de Hamming y guardarla
-    distancias[i] = hammingDist(h, h2);
+    distancias[hammingDist(h, h2)]++;
     totalDH += distancias[i];
     
   }
 
   //Calcular distancia de Hamming media
-  double dhmedia = (double) totalDH / (double) numIteraciones;
+  double dhmedia = (double) totalDH / (double) itsTotales;
   
       
   // Imprimir contadores
+  printf("Distancias de Hamming entre hashes alterando 1 bit del texto:\n");
   for(j=0;j<161;j++){
-    printf("Distancia Hamming entre hashes alterando 1 bit del texto = %d: %d veces.\n", j, contDH[j]);
+    printf("%d: %d veces.\n", j, distancias[j]);
   }
+  printf("La distancia de Hamming media es %f\n", dhmedia);
 
   // Liberar memoria asignada
   free(x);
