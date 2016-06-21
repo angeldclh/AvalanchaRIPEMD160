a71a66c99e84b9e232a14f2642bed00584c8de6c
Ángel <angel7delcastillo@gmail.com>
2016-06-21 15:40:48 +0200

    mallocs seguros

--- avalancha.c
+++ avalancha.c
@@ -5,8 +5,8 @@
 #include <openssl/evp.h>
 #include <string.h>
 
-#define NUMBITS 160   //Número de bits del texto en claro
-#define HASHSIZE 20  //Número de bytes del hash (160 bits)
+#define RANDOMSIZE 20   //Número de bytes de los números aleatorios a generar
+#define HASHSIZE 20     //Número de bytes del hash (160 bits)
 
 // Calcular distancia de Hamming de dos unsigned int: número de 1s de su XOR a nivel de bit
 int hammingDist(unsigned int a, unsigned int b){ 
@@ -18,68 +18,80 @@ int main(int argc, char **argv){
 	
 	// Comprobar número argumentos
 	if(argc > 2){
-		fprintf(stderr, "Número incorrecto de argumentos.\n./avalancha [numIteraciones]\n");
+		fprintf(stderr, "Número incorrecto de argumentos.\n./avalancha [numRondas]\n");
 		return 1;
 	}
 
-	// Hayar número iteraciones y bits del texto a cambiar en cada iteración
-	int numIteraciones;
+	// Hayar número rondas
+	int numRondas;
 	if(argc == 1){
-		numIteraciones = 160; //PONER ESTE VALOR BIEN, ESTÁ INVENTADO. TAMBIÉN EN EL PRINTF
-		printf("No se especifican argumentos, se asumen 160 iteraciones (número de bits del hash).\n");
+		numRondas = 1; 
+		printf("No se especifican argumentos, se asume 1 ronda.\n");
 	}
 	else{
-		numIteraciones = atoi(argv[1]);
+		numRondas = atoi(argv[1]);
 	}
 
 
 	// Buffers donde guardar los bits aleatorios generados (VER LONGITUD) y sus hashes
-	unsigned char *x = (unsigned char*)malloc(NUMBITS/8);
-	unsigned char *h = (unsigned char*)malloc(HASHSIZE); //hash: 160b=20B. 
-	unsigned char *x2 = (unsigned char*)malloc(NUMBITS/8);
-	unsigned char *h2 = (unsigned char*)malloc(HASHSIZE); //hash: 160b=20B. 
+	unsigned char *x;
+	unsigned char *h;
+	unsigned char *x2;
+	unsigned char *h2;
+	// Para cambiar 1 bit y para el popcount mejor unsigned int
+	unsigned int *ix;
+	unsigned int *ix2;
+	unsigned int *ih;
+	unsigned int *ih2;
 
-	unsigned int *ix = (unsigned int*)malloc(NUMBITS/8);
-	unsigned int *ix2 = (unsigned int*)malloc(NUMBITS/8);	
-	
-	unsigned int *ih = (unsigned int*)malloc(NUMBITS/8/*HASHSIZE*/);
-	unsigned int *ih2 = (unsigned int*)malloc(NUMBITS/8/*HASHSIZE*/);	
+	if(
+	   ((x = (unsigned char*)malloc(RANDOMSIZE)) == NULL) ||
+	   ((h = (unsigned char*)malloc(HASHSIZE)) == NULL) || //hash: 160b=20B. 
+	   ((x2 = (unsigned char*)malloc(RANDOMSIZE)) == NULL) ||
+	   ((h2 = (unsigned char*)malloc(HASHSIZE)) == NULL) || //hash: 160b=20B. 
+	   ((ix = (unsigned int*)malloc(RANDOMSIZE)) == NULL) ||
+	   ((ix2 = (unsigned int*)malloc(RANDOMSIZE)) == NULL) ||
+	   ((ih = (unsigned int*)malloc(HASHSIZE)) == NULL) ||
+	   ((ih2 = (unsigned int*)malloc(HASHSIZE)) == NULL)){
+		
+		fprintf(stderr, "Error en malloc.\n");
+		free(x);
+		free(h);
+		free(x2);
+		free(h2);
+		free(ix);
+		free(ix2);
+		free(ih);
+		free(ih2);
+	}
 	
 	/*distancias[z] contiene las veces que la distancia de Hamming entre hashes ha sido igual a z. Al tener el hash 160 bits,
 	  la distancia estará siempre en el intervalo [0,160]*/
 	int *distancias = (int*) malloc(161*sizeof(int));
 
-	int itsTotales = numIteraciones*NUMBITS;
-	int i, totalDH=0;
+	int itsTotales = numRondas*RANDOMSIZE*8;
+	int i, dist, totalDH=0;
 	/* Cada iteración altera un bit distinto del texto en claro. Tantas como indique el parámetro * nº bits de texto
 	   en claro (NUMBITS)*/
 	for(i=0;i<itsTotales;i++){
 		// Calcular aleatoriamente el número
-		RAND_bytes(x, NUMBITS/8);
-
-		memcpy(ix,x,NUMBITS/8);
+		RAND_bytes(x, RANDOMSIZE);
+		// Copiarlo a un unsigned int y cambiar 1 bit para calcular ambos hashes
+		memcpy(ix,x,RANDOMSIZE);
 		*ix2 = *ix^(1<<(i%NUMBITS));
-		
-		// Alterar 1 bit de x para obtener x2. 
-		//		*x2 = *x ^= 1 << (i%NUMBITS);
+		memcpy(x2,ix2,RANDOMSIZE);
 
-		////////////////////////////////////////
-		//memcpy(ih,x,NUMBITS/8);
-		//memcpy(ih2,x2,NUMBITS/8);
-		int d = hammingDist(*ix,*ix2);
-		printf("Debería ser 1 y es %d\n",d);
-		return 1;
-		////////////////////////////////////////
+
+
 		// Hash de x y de x2
 		/*h = hash(x);
 		  h2 = hash(x2);
 		*/
-		ih = (unsigned int *) &h;
-		ih2 = (unsigned int *) &h2;
 		
 		// Calcular distancia de Hamming y guardarla
-		distancias[hammingDist(*ih, *ih2)]++;
-		totalDH += distancias[i];
+		dist = hammingDist(*ih, *ih2);
+		distancias[dist]++;
+		totalDH += dist;
     
 	}
 
